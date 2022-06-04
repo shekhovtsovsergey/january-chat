970938ac136d2a67fe356ca5ad7d9d84d3ea2880
shekhovtsovsergey <98221024+shekhovtsovsergey@users.noreply.github.com>
2022-06-04 19:11:18 +0300

    Lesson4

--- chat-server/src/main/java/ru/geekbrains/january_chat/chat_server/App.java
+++ chat-server/src/main/java/ru/geekbrains/january_chat/chat_server/App.java
@@ -5,6 +5,6 @@ import ru.geekbrains.january_chat.chat_server.server.Server;
 
 public class App {
     public static void main(String[] args) {
-        new Server(new InMemoryAuthService()).start();
+        new Server(new InMemoryAuthService(), executorService).start();
     }
 }
